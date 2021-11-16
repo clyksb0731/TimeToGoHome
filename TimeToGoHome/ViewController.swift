3de77c0e2d653d3957e0d42216359a6da08abff1
Yongseok Choi <clyksb0731@gmail.com>
2021-11-17 00:16:39 +0900

    Initial Commit

---
+++ TimeToGoHome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimeToGoHome
+//
+//  Created by Yongseok Choi on 2021/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
