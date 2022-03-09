ad90ca2ee531dc964ee45b065fcdd8ed9f567449
Yongseok Choi <clyksb0731@gmail.com>
2022-03-09 12:25:50 +0900

    Add TemplateCell.

---
+++ TimeToGoHome/TemplateCell.swift
@@ -0,0 +1,84 @@
+//
+//  TemplateCell.swift
+//  TimeToGoHome
+//
+//  Created by Yongseok Choi on 2022/03/09.
+//
+
+import UIKit
+
+class TemplateCell: UITableViewCell {
+    
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.setCellFoundation()
+        self.initializeViews()
+        self.setGestures()
+        self.setNotificationCenters()
+        self.setSubviews()
+        self.setLayouts()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
+
+// MARK: Extension for essential methods
+extension TemplateCell {
+    // Set view foundation
+    func setCellFoundation() {
+        self.selectionStyle = .none
+        self.contentView.isUserInteractionEnabled = true
+    }
+    
+    // Initialize views
+    func initializeViews() {
+        
+    }
+    
+    // Set gestures
+    func setGestures() {
+        
+    }
+    
+    // Set notificationCenters
+    func setNotificationCenters() {
+        
+    }
+    
+    // Set subviews
+    func setSubviews() {
+        
+    }
+    
+    // Set layouts
+    func setLayouts() {
+        let safeArea = self.safeAreaLayoutGuide
+        
+        //  layout
+        NSLayoutConstraint.activate([
+            
+        ])
+    }
+}
+
+// MARK: - Methods added
+extension TemplateCell {
+    func setCell(_ address: String, isSelected: Bool, tag: Int) {
+        
+    }
+}
