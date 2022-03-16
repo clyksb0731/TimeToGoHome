72feea8f99f2a7673caa2efcea4aad42c87b52e6
Yongseok Choi <clyksb0731@gmail.com>
2022-03-16 12:29:27 +0900

    Update main cover view controller. Add image, close normal schedule button.

--- TimeToGoHome/MainCoverViewController.swift
+++ TimeToGoHome/MainCoverViewController.swift
@@ -24,10 +24,120 @@ enum MainCoverType {
 
 class MainCoverViewController: UIViewController {
     
+    var baseView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .useRGB(red: 0, green: 0, blue: 0, alpha: 0.3)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    // Case 1 - normal schedule type
+    lazy var normalScheduleBaseView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 10
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    lazy var normalScheduleTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 21)
+        label.textAlignment = .center
+        label.textColor = .black
+        label.text = "일정 변경"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    lazy var normalScheduleListView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    lazy var workButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .useRGB(red: 125, green: 243, blue: 110)
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle("근무", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 24, weight: .semibold)
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(workButton(_:)), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    lazy var vacationButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .useRGB(red: 120, green: 223, blue: 238)
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle("휴가", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 24, weight: .semibold)
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(vacationButton(_:)), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    lazy var holidayButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .useRGB(red: 252, green: 247, blue: 143)
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle("휴일", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 24, weight: .semibold)
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(holidayButton(_:)), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    lazy var closeNormalScheduleListViewButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "closeNormalScheduleListViewButtonImage"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    
+    // Case 2 - overtime schedule type
+    lazy var overtimeScheduleBaseView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 10
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    // Case 3 - starting work time type
+    lazy var startingWorkTimeBaseView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 10
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    var mainCoverType: MainCoverType
+    
     var delegate: MainCoverDelegate?
     
-    init(mainCoverTypeFor: ScheduleType) {
+    init(mainCoverTypeFor coverType: MainCoverType) {
+        self.mainCoverType = coverType
+        
         super.init(nibName: nil, bundle: nil)
+        
+        self.modalPresentationStyle = .overCurrentContext
     }
     
     required init?(coder: NSCoder) {
@@ -75,7 +185,8 @@ extension MainCoverViewController {
 extension MainCoverViewController {
     // Set view foundation
     func setViewFoundation() {
-        
+        self.view.backgroundColor = .clear
+        self.view.isOpaque = false
     }
     
     // Initialize views
@@ -116,6 +227,16 @@ extension MainCoverViewController {
 
 // MARK: - Extension for Selector methods
 extension MainCoverViewController {
+    @objc func workButton(_ sender: UIButton) {
+        
+    }
     
+    @objc func vacationButton(_ sender: UIButton) {
+        
+    }
+    
+    @objc func holidayButton(_ sender: UIButton) {
+        
+    }
 }
 
