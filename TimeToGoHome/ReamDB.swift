70e51e70e809c48f1a07de51bb11e1dc9da090bf
Yongseok Choi <clyksb0731@gmail.com>
2022-10-25 16:20:42 +0900

    Update compony and schedule db scheme. Add vacation db scheme.

--- TimeToGoHome/ReamDB.swift
+++ TimeToGoHome/ReamDB.swift
@@ -9,8 +9,7 @@ import UIKit
 import RealmSwift
 
 class Company: Object {
-    @Persisted(primaryKey: false) var joiningDateId: String = ""
-    @Persisted var joingingDate:Date = Date()
+    @Persisted(primaryKey: false) var dateId: String = ""
     @Persisted var name: String = ""
     @Persisted var address: String = ""
     @Persisted var latitude: Double = 0
@@ -18,17 +17,10 @@ class Company: Object {
     @Persisted var isCurrent: Bool = false
     @Persisted var schedule: List<Schedule>
     
-    convenience init(joiningDateId: String,
-         joingingDate: Date,
-         name: String,
-         address: String,
-         latitude: Double,
-         longitude: Double,
-         isCurrent: Bool) {
+    convenience init(joiningDateId dateId: String, name: String, address: String, latitude: Double, longitude: Double, isCurrent: Bool) {
         self.init()
 
-        self.joiningDateId = joiningDateId
-        self.joingingDate = joingingDate
+        self.dateId = dateId
         self.name = name
         self.address = address
         self.latitude = latitude
@@ -39,8 +31,21 @@ class Company: Object {
 
 class Schedule: EmbeddedObject {
     @Persisted(primaryKey: true) var dateId: String = ""
-    @Persisted var date: Date = Date()
     @Persisted var morning: String = ""
     @Persisted var afternoon: String = ""
-    @Persisted var overtime: Double = 0
+    @Persisted var overtime: Int = 0
+    
+    convenience init(dayDateId dateId: String, morningType morning: WorkTimeType, afternoonType afternoon: WorkTimeType, overtime: Int) {
+        self.init()
+        
+        self.dateId = dateId
+        self.morning = morning.rawValue
+        self.afternoon = afternoon.rawValue
+        self.overtime = overtime
+    }
+}
+
+class Vacation: Object {
+    @Persisted(primaryKey: true) var dateId: String = ""
+    @Persisted var vacationType: String = VacationType.none.rawValue
 }
