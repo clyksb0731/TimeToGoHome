8f8f8f9ea47a85c36f5fbaa762e92fca6ed3131b
Yongseok Choi <clyksb0731@gmail.com>
2022-12-20 23:55:03 +0900

    Group MenuCover. Add large menu cell but not completed.

---
+++ TimeToGoHome/Menu/MenuLargeCell.swift
@@ -0,0 +1,23 @@
+//
+//  MenuLargeCell.swift
+//  TimeToGoHome
+//
+//  Created by Yongseok Choi on 2022/12/20.
+//
+
+import UIKit
+
+class MenuLargeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
