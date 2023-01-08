32fa5f4c094fe022b730f35ef78ff112be29d281
Yongseok Choi <clyksb0731@gmail.com>
2023-01-08 20:27:32 +0900

    Add setting company map VC and setting holidays VC.

---
+++ TimeToGoHome/Setting/SettingCompanyMap/SettingCompanyAddressCell.swift
@@ -0,0 +1,23 @@
+//
+//  SettingCompanyAddressCell.swift
+//  TimeToGoHome
+//
+//  Created by Yongseok Choi on 2023/01/08.
+//
+
+import UIKit
+
+class SettingCompanyAddressCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
