149a951cdb56c5016de676d7951a7d1408812419
Yongseok Choi <clyksb0731@gmail.com>
2023-01-13 03:57:35 +0900

    Add finding location using keyword.

--- TimeToGoHome/Models/CompanyLocationModel.swift
+++ TimeToGoHome/Models/CompanyLocationModel.swift
@@ -9,8 +9,36 @@ import Foundation
 import Alamofire
 
 struct CompanyLocationModel {
+    private(set) var findKeywordWithTextRequest: DataRequest?
     private(set) var searchAddressWithTextReqeust: DataRequest?
     private(set) var findAddressWithCenterRequest: DataRequest?
+    
+    mutating func findKeywordWithTextRequest(_ text: String, page: Int, success: ((KeywordResult) -> ())?, failure: (() -> ())?) {
+        let url = "https://dapi.kakao.com/v2/local/search/keyword.json"
+        
+        let headers: HTTPHeaders = [
+            "Authorization":ReferenceValues.kakaoAuthKey
+        ]
+        
+        let parameters: Parameters = [
+            "query":text, // 검색을 원하는 질의어
+            "page":page, // 결과 페이지 번호: 1~45 사이의 값 (기본값: 1)
+        ]
+        
+        self.findKeywordWithTextRequest = AF.request(url, method: .get, parameters: parameters, encoding: URLEncoding.default, headers: headers)
+        
+        self.findKeywordWithTextRequest?.responseDecodable(of: KeywordResult.self) { response in
+            switch response.result {
+            case .success(let decodedData):
+                print("findKeywordWithTextRequest succeeded: \(decodedData)")
+                success?(decodedData)
+                
+            case .failure(let error):
+                print("findKeywordWithTextRequest error: \(error.localizedDescription)")
+                failure?()
+            }
+        }
+    }
     
     mutating func searchAddressWithTextReqeust(_ text: String, success: ((CompanyAddress) -> ())?, failure: (() -> ())?) {
         let url = "https://dapi.kakao.com/v2/local/search/address.json"
@@ -62,6 +90,67 @@ struct CompanyLocationModel {
             case .failure(let error):
                 print("findAddressWithCenterRequest error: \(error.localizedDescription)")
             }
+        }
+    }
+}
+
+struct KeywordResult: Codable {
+    let meta: Meta
+    let documents: [Document]
+    
+    struct Meta: Codable {
+        let totalCount: Int // 검색어에 검색된 문서 수
+        let pageableCount: Int // total_count 중 노출 가능 문서 수 (최대값: 45?)
+        let isEnd: Bool // 현재 페이지가 마지막 페이지인지 여부, 값이 false이면 page를 증가시켜 다음 페이지 요청 가능
+        let regionInfo: RegionInfo // 질의어의 지역 및 키워드 분석 정보
+        
+        enum CodingKeys: String, CodingKey {
+            case totalCount = "total_count"
+            case pageableCount = "pageable_count"
+            case isEnd = "is_end"
+            case regionInfo = "same_name"
+        }
+        
+        struct RegionInfo: Codable {
+            let region: [String] // 질의어에서 인식된 지역의 리스트, 예: '중앙로 맛집' 에서 중앙로에 해당하는 지역 리스트
+            let keyword: String // 질의어에서 지역 정보를 제외한 키워드, 예: '중앙로 맛집' 에서 '맛집'
+            let selectedRegion: String // 인식된 지역 리스트 중, 현재 검색에 사용된 지역 정보
+            
+            enum CodingKeys: String, CodingKey {
+                case region
+                case keyword
+                case selectedRegion = "selected_region"
+            }
+        }
+    }
+    
+    struct Document: Codable {
+        let id: String // 장소 ID
+        let placeName: String // 장소명, 업체명
+        let categoryName: String // 카테고리 이름
+        let categoryGroupCode: String // 중요 카테고리만 그룹핑한 카테고리 그룹 코드
+        let categoryGroupName: String // 중요 카테고리만 그룹핑한 카테고리 그룹명
+        let phone: String // 전화번호
+        let addressName: String // 전체 지번 주소
+        let roadAddressName: String // 전체 도로명 주소
+        let latitude: String // Y 좌표값, 경위도인 경우 latitude(위도)
+        let longitude: String // X 좌표값, 경위도인 경우 longitude (경도)
+        let placeUrl: String // 장소 상세페이지 URL
+        let distance: String // 중심좌표까지의 거리 (단, x,y 파라미터를 준 경우에만 존재), 단위 meter
+        
+        enum CodingKeys: String, CodingKey {
+            case id
+            case placeName = "place_name"
+            case categoryName = "category_name"
+            case categoryGroupCode = "category_group_code"
+            case categoryGroupName = "category_group_name"
+            case phone
+            case addressName = "address_name"
+            case roadAddressName = "road_address_name"
+            case latitude = "y"
+            case longitude = "x"
+            case placeUrl = "place_url"
+            case distance
         }
     }
 }
