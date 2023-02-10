e6c6c2598b647da5f25e7937193fc8f751d024be
Yongseok Choi <clyksb0731@gmail.com>
2023-02-10 15:55:22 +0900

    Update handling main vc after initial setting.

--- TimeToGoHome/InitialSetting/DayOff/DayOffViewController.swift
+++ TimeToGoHome/InitialSetting/DayOff/DayOffViewController.swift
@@ -1631,18 +1631,10 @@ extension DayOffViewController {
         
         self.completeInitialSettings()
         
-        let mainVC = MainViewController()
-        mainVC.modalPresentationStyle = .fullScreen
-        // let mainNaviVC = CustomizedNavigationController(rootViewController: mainVC)
+        let mainNaviVC = CustomizedNavigationController(rootViewController: MainViewController())
+        mainNaviVC.modalPresentationStyle = .fullScreen
         
-//        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
-//            windowScene.windows.first?.rootViewController = mainNaviVC
-//            windowScene.windows.first?.makeKeyAndVisible()
-//        }
-        
-        // self.present(mainNaviVC, animated: true)
-        
-        self.present(mainVC, animated: true)
+        self.present(mainNaviVC, animated: true)
         
         self.vacationNotification?.invalidate()
     }
